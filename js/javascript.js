e9f825682636552fbacc2e182da407e836551a80
Dario <dario.salle@gmail.com>
2017-11-07 14:13:48 +0100

    First commit

---
+++ js/javascript.js
@@ -0,0 +1,32 @@
+var URL = "http://dati.lazio.it/catalog/api/action/datastore_search?resource_id=4f0fb401-43c2-45ab-a884-8bd543c34cf1&limit=3"
+var months = ['GENNAIO', 'FEBBRAIO', 'MARZO', 'APRILE', 'MAGGIO', 'GIUGNO', 'LUGLIO', 'AGOSTO', 'SETTEMBRE', 'OTTOBRE', 'NOVEMBRE', 'DICEMBRE']
+
+var material;
+
+$.getJSON(URL, function (data) {
+    // console.log(data);
+
+    TESTER = document.getElementById('tester');
+    Plotly.plot(TESTER, data.result.records.map(function (el) {
+        return {
+            x: months,
+            y: months.map(function (month) {
+                return parseInt(el[month]);
+                if (parseInt(el[month]) == null) {
+                    parseInt(el[month]) = 0;
+                };
+            }),
+            mode: 'lines',
+            connectgaps: true,
+            name: el.MATERIALE,
+            line: {
+                shape: 'spline',
+                width: 3,
+            },
+        };
+    }), {
+        margin: {
+            t: 30
+        }
+    });
+});
